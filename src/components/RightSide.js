2f113a69dfb6a2ea0b071c132b603b89480bc217
Vedant Gour <vedantgour45@gmail.com>
2023-11-01 13:28:04 +0530

    done

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -24,7 +24,7 @@ const RightSide = ({ data }) => {
       })
       .catch((error) => {
         console.log(error.message);
-        setLoading(false);
+        // setLoading(false);
       });
   }, []);
 
