b011fe6629ce8c3c71bba38d27da754adba14ac6
Vedant Gour <vedantgour45@gmail.com>
2023-11-01 13:26:13 +0530

    error handling

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -23,7 +23,7 @@ const RightSide = ({ data }) => {
         setLoading(false);
       })
       .catch((error) => {
-        setNews(error);
+        setNews(error.message);
         setLoading(false);
       });
   }, []);
